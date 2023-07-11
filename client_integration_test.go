3817b0243ecf31610a1d6ff9d6a6e800fed94d11
nzdjb <github@2a1b.com>
2023-07-11 08:42:28 +0000

    test: Add more integration tests.

--- client_integration_test.go
+++ client_integration_test.go
@@ -32,17 +32,147 @@ func TestIntegrationConfigureZone(t *testing.T) {
 			Data: "example.org",
 		},
 	}, nil)
-	if err != nil {
-		panic(err)
-	}
+	assert.NoError(t, err)
 }
 
 func TestIntegrationDnsZone(t *testing.T) {
 	c := NewMetanameClient(testAccountReference, testAccountAPIKey)
 	c.Host = "https://test.metaname.net/api/1.1"
 	result, err := c.DnsZone(context.TODO(), "testzone.nz")
+	assert.NoError(t, err)
+	noRefResult := []ResourceRecord{}
+	for _, r := range result {
+		assert.NotNil(t, r.Reference)
+		assert.NotEqual(t, "", *r.Reference)
+		r.Reference = nil
+		noRefResult = append(noRefResult, r)
+	}
+	aux := int(30)
+	assert.ElementsMatch(t, []ResourceRecord{{
+		Name: "george",
+		Type: "CNAME",
+		Aux:  nil,
+		Ttl:  600,
+		Data: "example.org",
+	},
+		{
+			Name: "mail",
+			Type: "MX",
+			Aux:  &aux,
+			Ttl:  600,
+			Data: "example.org",
+		},
+	}, noRefResult)
+}
+
+func TestIntegrationCreateDnsRecord(t *testing.T) {
+	c := NewMetanameClient(testAccountReference, testAccountAPIKey)
+	c.Host = "https://test.metaname.net/api/1.1"
+	ref, err := c.CreateDnsRecord(context.TODO(), "testzone.nz", ResourceRecord{
+		Name: "bill",
+		Type: "A",
+		Aux:  nil,
+		Ttl:  600,
+		Data: "127.0.0.1",
+	})
+	assert.NoError(t, err)
+	assert.NotEqual(t, "", ref)
+
+	result, err := c.DnsZone(context.TODO(), "testzone.nz")
+	noRefResult := []ResourceRecord{}
+	for _, r := range result {
+		assert.NotNil(t, r.Reference)
+		assert.NotEqual(t, "", *r.Reference)
+		r.Reference = nil
+		noRefResult = append(noRefResult, r)
+	}
+	aux := int(30)
+	assert.ElementsMatch(t, []ResourceRecord{{
+		Name: "george",
+		Type: "CNAME",
+		Aux:  nil,
+		Ttl:  600,
+		Data: "example.org",
+	},
+		{
+			Name: "mail",
+			Type: "MX",
+			Aux:  &aux,
+			Ttl:  600,
+			Data: "example.org",
+		},
+		{
+			Name: "bill",
+			Type: "A",
+			Aux:  nil,
+			Ttl:  600,
+			Data: "127.0.0.1",
+		},
+	}, noRefResult)
+}
+
+func TestIntegrationUpdateDnsRecord(t *testing.T) {
+	c := NewMetanameClient(testAccountReference, testAccountAPIKey)
+	c.Host = "https://test.metaname.net/api/1.1"
+	result, err := c.DnsZone(context.TODO(), "testzone.nz")
 	if err != nil {
 		panic(err)
 	}
-	assert.Equal(t, []ResourceRecord(nil), result)
+	for _, r := range result {
+		ref := *r.Reference
+		r.Reference = nil
+		r.Ttl = 300
+		err := c.UpdateDnsRecord(context.TODO(), "testzone.nz", ref, r)
+		assert.NoError(t, err)
+	}
+
+	result, err = c.DnsZone(context.TODO(), "testzone.nz")
+	noRefResult := []ResourceRecord{}
+	for _, r := range result {
+		assert.NotNil(t, r.Reference)
+		assert.NotEqual(t, "", *r.Reference)
+		r.Reference = nil
+		noRefResult = append(noRefResult, r)
+	}
+	aux := int(30)
+	assert.ElementsMatch(t, []ResourceRecord{{
+		Name: "george",
+		Type: "CNAME",
+		Aux:  nil,
+		Ttl:  300,
+		Data: "example.org",
+	},
+		{
+			Name: "mail",
+			Type: "MX",
+			Aux:  &aux,
+			Ttl:  300,
+			Data: "example.org",
+		},
+		{
+			Name: "bill",
+			Type: "A",
+			Aux:  nil,
+			Ttl:  300,
+			Data: "127.0.0.1",
+		},
+	}, noRefResult)
+}
+
+func TestIntegrationDeleteDnsRecord(t *testing.T) {
+	c := NewMetanameClient(testAccountReference, testAccountAPIKey)
+	c.Host = "https://test.metaname.net/api/1.1"
+	result, err := c.DnsZone(context.TODO(), "testzone.nz")
+	if err != nil {
+		panic(err)
+	}
+	for _, r := range result {
+		err = c.DeleteDnsRecord(context.TODO(), "testzone.nz", *r.Reference)
+		assert.NoError(t, err)
+	}
+	result, err = c.DnsZone(context.TODO(), "testzone.nz")
+	if err != nil {
+		panic(err)
+	}
+	assert.Empty(t, result)
 }
